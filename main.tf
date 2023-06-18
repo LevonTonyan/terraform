e4cc5b7ac4868577eff5aa0410a0a0913af63112
LevonTonyan <LevonTonyan87@gmail.com>
2023-06-18 11:26:49 +0400

    Add variables

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
     region = "us-east-2"
 } 
 
+variable "web_port" {
+    description = "port number to access server"
+  type = number
+
+}
+output "public_ip" {
+  value = aws_instance.terraform_demo.public_ip
+  description = "The public ip address of server"
+}
 
 resource "aws_instance" "terraform_demo" {
     ami = "ami-0e820afa569e84cc1"
@@ -13,7 +22,7 @@ resource "aws_instance" "terraform_demo" {
     user_data = <<-EOF
                     #!/bin/bash
                     echo "Hello from the other side" > index.xhtml
-                    nohup busybox httpd -f -p 8080 &
+                    nohup busybox httpd -f -p ${var.web_port} &
                     EOF
     
     user_data_replace_on_change = true
@@ -25,8 +34,8 @@ resource "aws_security_group" "webServer_SG" {
     name = "terraform-demo-sg"
 
     ingress  {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.web_port
+        to_port = var.web_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
